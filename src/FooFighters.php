aca16cb42a692a06117b1a67ff44faf49e30248e
Cameron Bingham <10758625@uvu.edu>
2019-02-25 18:53:10 -0700

    Final Commit

--- src/FooFighters.php
+++ src/FooFighters.php
@@ -1,12 +1,12 @@
 <?php
 class FooFighters {
-     public $foo;
-     public __construct(){
-          $this->foo = new array();
-          $this->foo[] = 'foo';
-     }
-     public getFoo() {
-          $random = random_int(0, count($this->foo)-1);
-          return $this->foo[$random];
-     }
+    public $foo;
+    public function __construct() {
+         $this->foo = array();
+         $this->foo[] = 'foo';
+    }
+    public function getFoo() {
+         $random = random_int(0, count($this->foo)-1);
+         return $this->foo[$random];
+    }
 }
